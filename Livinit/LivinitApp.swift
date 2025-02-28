047019860b4a0818c5b0434ceb7807a3cc780656
Lareina <sy796@cornell.edu>
2025-02-27 23:12:26 -0500

    Xcode init

---
+++ Livinit/LivinitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivinitApp.swift
+//  Livinit
+//
+//  Created by Lareina Yang on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LivinitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LivinitTests/LivinitTests.swift
@@ -0,0 +1,17 @@
+//
+//  LivinitTests.swift
+//  LivinitTests
+//
+//  Created by Lareina Yang on 2/27/25.
+//
+
+import Testing
+@testable import Livinit
+
+struct LivinitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
