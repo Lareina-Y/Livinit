8aa5717d3d8d64b1e528efc787d74165fc6b01a9
Lareina <sy796@cornell.edu>
2025-03-15 01:23:08 -0400

    Implete SplashView

--- Livinit/Views/SplashView.swift
+++ Livinit/Views/SplashView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var isAnimating = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Color.white.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("livinit-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .scaleEffect(isAnimating ? 1.0 : 0.8)
+                    .opacity(isAnimating ? 1.0 : 0.5)
+            }
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 1.2)) {
+                isAnimating = true
+            }
+        }
     }
 }
 
